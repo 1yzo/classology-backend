bfe922166dba63ffd1df5d0d5f543bc5fa4dcb87
Osman Ali <okali@uci.edu>
2018-12-05 22:03:08 -0800

    Finish /auth/signup route

--- routes/auth.js
+++ routes/auth.js
@@ -13,29 +13,33 @@ const router = express.Router();
 router.post('/signup', (req, res) => {
     const { email, password, name, role } = req.body;
 
+    if (role === 'STUDENT') {
+        if (!req.body.schoolId) {
+            res.status(400).json('SchoolId is required for Students');
+        }
+    }
+
     const user = new User({
         email,
         password,
         role
     });
-
     user.save()
         .then(() => {
             if (role === 'TEACHER') {
                const teacher = new Teacher({ name });
                return teacher.save();
-            } else if (role === 'STUDENT') {
+            }
+            if (role === 'STUDENT') {
                 const student = new Student({
                     name,
                     schoolId: req.body.schoolId
                 });
                 return student.save();
-            } else {
-                res.status(500).send(`"${role}" is not a valid role`);
             }
         })
-        .then(entity => res.json(entity))
-        .catch(err => res.status(500).send(err));
+        .then( entity => res.json('Success, user created!'))
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
